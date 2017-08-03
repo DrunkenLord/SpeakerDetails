8ecac82e27cbeaf211cac60fb1ac0dbb8d5817d5
Mehul Singhal <mehulbtp@gmail.com>
2017-08-04 01:57:51 +0530

    Initial Commit

---
+++ SpeakerDetails/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeakerDetails
+//
+//  Created by Mehul  Singhal  on 04/08/17.
+//  Copyright © 2017 Mehul  Singhal . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
